1d7f107d776c291254bf5209e82073bedcad538c
NeelOommen <neelittyerahoommen@gmail.com>
2022-09-03 23:01:11 +0530

    Added Base Pages
    Started the creation of the main page, as well as 'not found' pages for the clubs and events links

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: "'Montserrat', sans-serif",
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'mainColor': '#071e3d',
+      'mainAccent': '#205091',
+      'blueAccent1': '#006186',
+      'blueAccent2': '#00ADB8',
+      'blueAlternate1': '#86b6fc',
+      'whiteAlternate': '#F7F9FF',
+      'sandAlternate': '#F1F1E6',
+      'white': '#ffffff'
+    },
   },
   plugins: [],
 }
